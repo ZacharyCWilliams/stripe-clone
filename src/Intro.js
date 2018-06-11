74e02f96de82304f1b828b51eb4d8cea86a44faa
Zachary C Williams <ZachCWilliams@icloud.com>
2018-06-10 21:23:42 -0700

    Intro buttons get their own component
    type: refactor Intro.js into multiple components
    
    <IntroButtons />

--- src/Intro.js
+++ src/Intro.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
+import Announcement from './Announcement.js'
+import IntroButtons from './IntroButtons.js'
 import {
   BrowserRouter as Router,
   Route,
@@ -11,16 +13,13 @@ function Intro(props){
       <section id="intro">
         <div className="intro-container">
           <Announcement />
-          <h1 className="intro-h1">The new standard in online payments</h1>
+          <h1 className="intro-h1">
+            The new standard in online payments
+          </h1>
           <p className="intro-p">
             Stripe is the best software platform for running an internet business. We handle billions of dollars every year for forward-thinking businesses around the world.
           </p>
-          <Router>
-            <ul id="Menu2-Ul">
-              <li className="menu2-itemid" className="button1-hover" id="menu2-itemid" className="menu2-item1class"><Link to="/register" className="menu2-item" id="menu2-itemid">Create Account</Link></li>
-              <li id="menu2-item2id" className="button-hover"><Link to="/contact/sales" className="menu2-item" id="menu2-item2id">Contact Sales</Link></li>
-            </ul>
-          </Router>
+          <IntroButtons />
         </div>
       </section>
     );
