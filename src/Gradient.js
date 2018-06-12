13ebfe758276a614dc3d9589a7849492d3bbfa37
Zachary C Williams <ZachCWilliams@icloud.com>
2018-06-12 12:20:38 -0700

    Create Gradient.js
    type: create Gradient.js for Stripe gradient variations
    
    background gradient variations

---
+++ src/Gradient.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import './App.css';
+
+  function Gradient(props) {
+    return (
+          <div id="stripes" aria-hidden="true">
+            <span id="gradient-span-1"></span>
+            <span id="gradient-span-2"></span>
+            <span id="gradient-span-3"></span>
+            <span id="gradient-span-4"></span>
+            <span id="gradient-span-5"></span>
+          </div>
+    );
+  }
+
+export default Gradient;
