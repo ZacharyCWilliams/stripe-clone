835524ab1c809489ab6ba5afb1d03e1da7f00019
Zachary C Williams <ZachCWilliams@icloud.com>
2018-06-12 12:19:21 -0700

    Add Gradient.js to App.js
    type: add Component to App.js
    
    Previously made gradient precise. Now adding Gradient.js to add Stripe gradient variations

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import './App.css';
 import Menu from './Menu.js'
 import Intro from './Intro.js'
+import Gradient from './Gradient.js'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
+          <Gradient />
           <Menu />
           <Intro />
         </header>
